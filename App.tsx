438eb321cea73b111b889c2a0eb168fbfeeca097
jotatks772 <jotatk@proton.me>
2025-12-25 23:09:26 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import CheckoutPage from './components/CheckoutPage';
-import AdminPanel from './components/AdminPanel';
-
-const Nav: React.FC = () => (
-  <nav className="bg-gray-800 p-2 text-white fixed top-0 right-0 z-50 text-xs rounded-bl-lg">
-    <Link to="/" className="hover:underline p-2">Target View</Link>
-    <Link to="/admin" className="hover:underline p-2">Admin View</Link>
-  </nav>
-);
 
 function App() {
   return (
     <HashRouter>
-      <Nav />
+      {/* O componente de Navegação foi removido pois não é mais necessário */}
       <Routes>
         <Route path="/" element={<CheckoutPage />} />
-        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </HashRouter>
   );
