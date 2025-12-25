58079ee5fa9a19838952d3132e8069f5a621fcaa
jotatks772 <jotatk@proton.me>
2025-12-25 23:23:05 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import CheckoutPage from './components/CheckoutPage';
+// Importe a nova página de admin do seu diretório isolado
+import AdminPage from './admin/AdminPage';
 
 function App() {
   return (
     <HashRouter>
-      {/* O componente de Navegação foi removido pois não é mais necessário */}
+      {/* Nenhum link de navegação visível para o admin */}
       <Routes>
+        {/* A rota principal continua sendo a página de checkout */}
         <Route path="/" element={<CheckoutPage />} />
+        
+        {/* A rota /admin agora renderiza o componente AdminPage */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </HashRouter>
   );
